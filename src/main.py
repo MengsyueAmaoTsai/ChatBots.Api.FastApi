7ebc4fa02e8f022c4c64f3dbaf7c194ac6d2a7a0
Mengsyue Amao Tsai <mengsyue.tsai@outlook.com>
2024-09-25 09:48:39 +0800

    Update vscode settings

--- src/main.py
+++ src/main.py
@@ -1,6 +1,8 @@
-import uvicorn
 from fastapi import FastAPI
 
 app = FastAPI()
 
-uvicorn.run(app, host="0.0.0.0", port=8000)
+if __name__ == "__main__":
+    import uvicorn
+
+    uvicorn.run(app, host="0.0.0.0", port=8000)
