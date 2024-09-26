866fbdce513ace29f24be758c3e1ff3cea50e9f2
Mengsyue Amao Tsai <mengsyue.tsai@outlook.com>
2024-09-26 13:46:02 +0800

    Add framework module

---
+++ src/web.py
@@ -0,0 +1,36 @@
+from argparse import ArgumentParser
+
+import uvicorn
+from fastapi import FastAPI
+
+
+class WebApplication:
+    def __init__(self):
+        self.__app = FastAPI()
+
+    @staticmethod
+    def create_builder():
+        return WebApplicationBuilder()
+
+    def run(self):
+        parser = ArgumentParser()
+        parser.add_argument("--host", default="127.0.0.1", type=str)
+        parser.add_argument("--port", default=10002, type=int)
+        parser.add_argument("--watch", action="store_true")
+        parsed_args = parser.parse_args()
+
+        uvicorn.run(
+            "__main__:app",
+            host=parsed_args.host,
+            port=parsed_args.port,
+            reload=parsed_args.watch,
+        )
+
+
+class WebApplicationBuilder:
+    def __init__(self):
+        pass
+
+    def build(self):
+        web_app = WebApplication()
+        return web_app
