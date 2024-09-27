4b022be6885d75fa3d83fc7868f1111c872f49e0
Mengsyue Amao Tsai <mengsyue.tsai@outlook.com>
2024-09-27 17:39:02 +0800

    Fix tests

--- tests/unit_tests/shared_kernel/test_error.py
+++ tests/unit_tests/shared_kernel/test_error.py
@@ -1,65 +1,68 @@
 from shared_kernel import Error, ErrorType
 
+error_code = "error_code"
+error_message = "error_message"
+
 
 def test_error_create():
-    error = Error.create(ErrorType.Validation, "code", "message")
+    error = Error.create(ErrorType.Validation, error_code, error_message)
 
     assert error.type is ErrorType.Validation
-    assert error.code == "code"
-    assert error.message == "message"
+    assert error.code == error_code
+    assert error.message == error_message
 
 
 def test_error_invalid():
-    error = Error.invalid("code", "message")
+    error = Error.invalid(error_code, error_message)
 
     assert error.type is ErrorType.Validation
-    assert error.code == "code"
-    assert error.message == "message"
+    assert error.code == error_code
+    assert error.message == error_message
 
 
 def test_error_unauthorized():
-    error = Error.unauthorized("code", "message")
+    error = Error.unauthorized(error_code, error_message)
 
     assert error.type is ErrorType.Unauthorized
-    assert error.code == "code"
-    assert error.message == "message"
+    assert error.code == error_code
+    assert error.message == error_message
 
 
 def test_error_forbidden():
-    error = Error.forbidden("code", "message")
+    error = Error.forbidden(error_code, error_message)
 
     assert error.type is ErrorType.Forbidden
-    assert error.code == "code"
-    assert error.message == "message"
+    assert error.code == error_code
+    assert error.message == error_message
 
 
 def test_error_not_found():
-    error = Error.not_found("code", "message")
+    error = Error.not_found(error_code, error_message)
 
     assert error.type is ErrorType.NotFound
-    assert error.code == "code"
-    assert error.message == "message"
+    assert error.code == error_code
+    assert error.message == error_message
 
 
 def test_error_conflict():
-    error = Error.conflict("code", "message")
+    error = Error.conflict(error_code, error_message)
 
     assert error.type is ErrorType.Conflict
-    assert error.code == "code"
-    assert error.message == "message"
+    assert error.code == error_code
+    assert error.message == error_message
 
 
 def test_error_unexpected():
-    error = Error.unexpected("code", "message")
+    error = Error.unexpected(error_code, error_message)
 
     assert error.type is ErrorType.Unexpected
-    assert error.code == "code"
-    assert error.message == "message"
+    assert error.code == error_code
+    assert error.message == error_message
 
 
 def test_error_unavailable():
-    error = Error.unavailable("code", "message")
+    error = Error.unavailable(error_code, error_message)
 
     assert error.type is ErrorType.Unavailable
-    assert error.code == "code"
-    assert error.message == "message"
+    assert error.code == error_code
+    assert error.message == error_message
