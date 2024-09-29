b43b15717513ae783845322c69d4ac1c1f710b63
Mengsyue Amao Tsai <mengsyue.tsai@outlook.com>
2024-09-29 21:25:40 +0800

    reset

--- src/main.py
+++ src/main.py
@@ -1,30 +1,8 @@
-from domain import LineBotService
-from domain.abstractions import ILineBotService
-from hosting import WebApplication
-from infrastructure.resources import ResourceService
-from line.messaging import LineMessagingClient
+from builder import WebApplication
 
 builder = WebApplication.create_builder()
 
-## Common services
-builder.services.add_scoped(LineMessagingClient)
-
-## Domain services
-builder.services.add_scoped(ResourceService)
-
-## Application services
-builder.services.add_commands()
-builder.services.add_scoped(ILineBotService, LineBotService)
-
-
-## Presentation - Endpoints
-builder.services.add_endpoints()
-
 app = builder.build()
-
-app.use_request_debugging()
-
-app.map_endpoints()
 
 if __name__ == "__main__":
     app.run()
