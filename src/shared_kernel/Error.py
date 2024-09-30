a63ed0a8cea2bc73747b323aefa42d86ea18c9fc
Mengsyue Amao Tsai <mengsyue.tsai@outlook.com>
2024-09-30 08:23:33 +0800

    Error tes

--- src/shared_kernel/Error.py
+++ src/shared_kernel/Error.py
@@ -1,8 +1,10 @@
 from dataclasses import dataclass
 from enum import Enum
+from typing import Any, overload
 
 
 class ErrorType(Enum):
+    Null = 0
     Validation = 400
     Unauthorized = 401
     Forbidden = 403
@@ -19,5 +21,107 @@ class Error:
     message: str
 
     @staticmethod
+    def null() -> "Error":
+        return Error(ErrorType.Null, ErrorType.Null.name, "")
+
+    @staticmethod
     def create(error_type: ErrorType, code: str, message: str) -> "Error":
         return Error(error_type, code, message)
+
+    @overload
+    @staticmethod
+    def invalid(code: str, message: str) -> "Error": ...
+
+    @overload
+    @staticmethod
+    def invalid(message: str) -> "Error": ...
+
+    @staticmethod
+    def invalid(*args: Any, **kwargs: Any) -> "Error":
+        if len(args) == 1:
+            return Error(ErrorType.Validation, ErrorType.Validation.name, args[0])
+        return Error(ErrorType.Validation, args[0], args[1])
+
+    @overload
+    @staticmethod
+    def unauthorized(code: str, message: str) -> "Error": ...
+
+    @overload
+    @staticmethod
+    def unauthorized(message: str) -> "Error": ...
+
+    @staticmethod
+    def unauthorized(*args: Any, **kwargs: Any) -> "Error":
+        if len(args) == 1:
+            return Error(ErrorType.Unauthorized, ErrorType.Unauthorized.name, args[0])
+        return Error(ErrorType.Unauthorized, args[0], args[1])
+
+    @overload
+    @staticmethod
+    def forbidden(code: str, message: str) -> "Error": ...
+
+    @overload
+    @staticmethod
+    def forbidden(message: str) -> "Error": ...
+
+    @staticmethod
+    def forbidden(*args: Any, **kwargs: Any) -> "Error":
+        if len(args) == 1:
+            return Error(ErrorType.Forbidden, ErrorType.Forbidden.name, args[0])
+        return Error(ErrorType.Forbidden, args[0], args[1])
+
+    @overload
+    @staticmethod
+    def not_found(code: str, message: str) -> "Error": ...
+
+    @overload
+    @staticmethod
+    def not_found(message: str) -> "Error": ...
+
+    @staticmethod
+    def not_found(*args: Any, **kwargs: Any) -> "Error":
+        if len(args) == 1:
+            return Error(ErrorType.NotFound, ErrorType.NotFound.name, args[0])
+        return Error(ErrorType.NotFound, args[0], args[1])
+
+    @overload
+    @staticmethod
+    def conflict(code: str, message: str) -> "Error": ...
+
+    @overload
+    @staticmethod
+    def conflict(message: str) -> "Error": ...
+
+    @staticmethod
+    def conflict(*args: Any, **kwargs: Any) -> "Error":
+        if len(args) == 1:
+            return Error(ErrorType.Conflict, ErrorType.Conflict.name, args[0])
+        return Error(ErrorType.Conflict, args[0], args[1])
+
+    @overload
+    @staticmethod
+    def unexpected(code: str, message: str) -> "Error": ...
+
+    @overload
+    @staticmethod
+    def unexpected(message: str) -> "Error": ...
+
+    @staticmethod
+    def unexpected(*args: Any, **kwargs: Any) -> "Error":
+        if len(args) == 1:
+            return Error(ErrorType.Unexpected, ErrorType.Unexpected.name, args[0])
+        return Error(ErrorType.Unexpected, args[0], args[1])
+
+    @overload
+    @staticmethod
+    def unavailable(code: str, message: str) -> "Error": ...
+
+    @overload
+    @staticmethod
+    def unavailable(message: str) -> "Error": ...
+
+    @staticmethod
+    def unavailable(*args: Any, **kwargs: Any) -> "Error":
+        if len(args) == 1:
+            return Error(ErrorType.Unavailable, ErrorType.Unavailable.name, args[0])
+        return Error(ErrorType.Unavailable, args[0], args[1])
--- tests/unit_tests/shared_kernel/ErrorTests.py
+++ tests/unit_tests/shared_kernel/ErrorTests.py
@@ -41,3 +41,86 @@ class ErrorTests:
         assert_that(error.type).is_equal_to(ErrorType.Validation)
         assert_that(error.code).is_equal_to(ERROR_CODE)
         assert_that(error.message).is_equal_to(ERROR_MESSAGE)
+
+        validation_error = Error.invalid(ERROR_MESSAGE)
+        validation_error2 = Error.invalid(ERROR_CODE, ERROR_MESSAGE)
+
+        assert_that(validation_error.type).is_equal_to(ErrorType.Validation)
+        assert_that(validation_error.code).is_equal_to(ErrorType.Validation.name)
+        assert_that(validation_error.message).is_equal_to(ERROR_MESSAGE)
+
+        assert_that(validation_error2.type).is_equal_to(ErrorType.Validation)
+        assert_that(validation_error2.code).is_equal_to(ERROR_CODE)
+        assert_that(validation_error2.message).is_equal_to(ERROR_MESSAGE)
+
+        unauthorized_error = Error.unauthorized(ERROR_MESSAGE)
+        unauthorized_error2 = Error.unauthorized(ERROR_CODE, ERROR_MESSAGE)
+
+        assert_that(unauthorized_error.type).is_equal_to(ErrorType.Unauthorized)
+        assert_that(unauthorized_error.code).is_equal_to(ErrorType.Unauthorized.name)
+        assert_that(unauthorized_error.message).is_equal_to(ERROR_MESSAGE)
+
+        assert_that(unauthorized_error2.type).is_equal_to(ErrorType.Unauthorized)
+        assert_that(unauthorized_error2.code).is_equal_to(ERROR_CODE)
+        assert_that(unauthorized_error2.message).is_equal_to(ERROR_MESSAGE)
+
+        forbidden_error = Error.forbidden(ERROR_MESSAGE)
+        forbidden_error2 = Error.forbidden(ERROR_CODE, ERROR_MESSAGE)
+
+        assert_that(forbidden_error.type).is_equal_to(ErrorType.Forbidden)
+        assert_that(forbidden_error.code).is_equal_to(ErrorType.Forbidden.name)
+        assert_that(forbidden_error.message).is_equal_to(ERROR_MESSAGE)
+
+        assert_that(forbidden_error2.type).is_equal_to(ErrorType.Forbidden)
+        assert_that(forbidden_error2.code).is_equal_to(ERROR_CODE)
+        assert_that(forbidden_error2.message).is_equal_to(ERROR_MESSAGE)
+
+        not_found_error = Error.not_found(ERROR_MESSAGE)
+        not_found_error2 = Error.not_found(ERROR_CODE, ERROR_MESSAGE)
+
+        assert_that(not_found_error.type).is_equal_to(ErrorType.NotFound)
+        assert_that(not_found_error.code).is_equal_to(ErrorType.NotFound.name)
+        assert_that(not_found_error.message).is_equal_to(ERROR_MESSAGE)
+
+        assert_that(not_found_error2.type).is_equal_to(ErrorType.NotFound)
+        assert_that(not_found_error2.code).is_equal_to(ERROR_CODE)
+        assert_that(not_found_error2.message).is_equal_to(ERROR_MESSAGE)
+
+        conflict_error = Error.conflict(ERROR_MESSAGE)
+        conflict_error2 = Error.conflict(ERROR_CODE, ERROR_MESSAGE)
+
+        assert_that(conflict_error.type).is_equal_to(ErrorType.Conflict)
+        assert_that(conflict_error.code).is_equal_to(ErrorType.Conflict.name)
+        assert_that(conflict_error.message).is_equal_to(ERROR_MESSAGE)
+
+        assert_that(conflict_error2.type).is_equal_to(ErrorType.Conflict)
+        assert_that(conflict_error2.code).is_equal_to(ERROR_CODE)
+        assert_that(conflict_error2.message).is_equal_to(ERROR_MESSAGE)
+
+        unexpected_error = Error.unexpected(ERROR_MESSAGE)
+        unexpected_error2 = Error.unexpected(ERROR_CODE, ERROR_MESSAGE)
+
+        assert_that(unexpected_error.type).is_equal_to(ErrorType.Unexpected)
+        assert_that(unexpected_error.code).is_equal_to(ErrorType.Unexpected.name)
+        assert_that(unexpected_error.message).is_equal_to(ERROR_MESSAGE)
+
+        assert_that(unexpected_error2.type).is_equal_to(ErrorType.Unexpected)
+        assert_that(unexpected_error2.code).is_equal_to(ERROR_CODE)
+        assert_that(unexpected_error2.message).is_equal_to(ERROR_MESSAGE)
+
+        unavailable_error = Error.unavailable(ERROR_MESSAGE)
+        unavailable_error2 = Error.unavailable(ERROR_CODE, ERROR_MESSAGE)
+
+        assert_that(unavailable_error.type).is_equal_to(ErrorType.Unavailable)
+        assert_that(unavailable_error.code).is_equal_to(ErrorType.Unavailable.name)
+        assert_that(unavailable_error.message).is_equal_to(ERROR_MESSAGE)
+
+        assert_that(unavailable_error2.type).is_equal_to(ErrorType.Unavailable)
+        assert_that(unavailable_error2.code).is_equal_to(ERROR_CODE)
+        assert_that(unavailable_error2.message).is_equal_to(ERROR_MESSAGE)
+
+        null_error = Error.null()
+
+        assert_that(null_error.type).is_equal_to(ErrorType.Null)
+        assert_that(null_error.code).is_equal_to(ErrorType.Null.name)
+        assert_that(null_error.message).is_equal_to(str())
